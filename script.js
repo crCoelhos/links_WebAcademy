310c16ed9372657e23d3e211f5dd4f302c0e0335
crCoelhos <cristophersaar@gmail.com>
2022-06-27 18:10:24 -0500

    alteração para tornar a lista um link funcional abrindo em nova aba

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // Cria uma lista nova
 function novoLink() {
+    var a = document.createElement("a");
     var li = document.createElement("li");
     var inputValue = document.getElementById("entrada").value;
     var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    a.appendChild(t) //a gerado com o link
+    li.appendChild(a); //link linkado no item da lista
+    a.href = 'https://' + inputValue //mudar o href do elemento
+    a.target= "_blank" //propriedade para abrir o link em uma nova aba
+
+
     if (inputValue === '') { //validar se ta vazio ou não de forma porca
         document.getElementById('entrada').style['background'] = 'red'; //tem que fazer isso resetar assim que o elemento for inserido/ação for corrigida
         // alert("entrada invalida ou em branco");
     } else {
+        a.value = 'https://' + li.value
         document.getElementById("novaUL").appendChild(li);
     }
+    document.querySelector('a').style['text-decoration'] = 'none';
     document.getElementById("entrada").value = "";
 
-    var span = document.createElement("SPAN");
+    var span = document.createElement("span");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
